904fbd6b1921ecc152d868d6974851e5fe5f7e7e
Oikswt <50925610+neespi@users.noreply.github.com>
2020-06-04 01:59:25 +0300

    Add files via upload

---
+++ bridges-practice1/Bridges.WebService/ApplicationServices/Repositories/DbBridgesPointRepository.cs
@@ -0,0 +1,37 @@
+﻿using Bridges.ApplicationServices.Ports.Gateways.Database;
+using Bridges.DomainObjects;
+using Bridges.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Bridges.ApplicationServices.Repositories
+{
+    public class DbBridgesPointRepository : IReadOnlyBridgesPointRepository,
+                                     IBridgesPointRepository
+    {
+        private readonly IBridgesDatabaseGateway _databaseGateway;
+
+        public DbBridgesPointRepository(IBridgesDatabaseGateway databaseGateway)
+            => _databaseGateway = databaseGateway;
+
+        public async Task<BridgesPoint> GetBridgesPoint(long id)
+            => await _databaseGateway.GetBridgesPoint(id);
+
+        public async Task<IEnumerable<BridgesPoint>> GetAllBridgesPoints()
+            => await _databaseGateway.GetAllBridgesPoints();
+
+        public async Task<IEnumerable<BridgesPoint>> QueryBridgesPoints(ICriteria<BridgesPoint> criteria)
+            => await _databaseGateway.QueryBridgesPoints(criteria.Filter);
+
+        public async Task AddBridgesPoint(BridgesPoint bridgespoint)
+            => await _databaseGateway.AddBridgesPoint(bridgespoint);
+
+        public async Task RemoveBridgesPoint(BridgesPoint bridgespoint)
+            => await _databaseGateway.RemoveBridgesPoint(bridgespoint);
+
+        public async Task UpdateBridgesPoint(BridgesPoint bridgespoint)
+            => await _databaseGateway.UpdateBridgesPoint(bridgespoint);
+    }
+}
---
+++ bridges-practice1/Bridges.WebService/InfrastructureServices/Gateways/Database/BridgesContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bridges.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridges.InfrastructureServices.Gateways.Database
+{
+    public class BridgesContext : DbContext
+    {
+        public DbSet<BridgesPoint> BridgesPoints { get; set; }
+
+        public BridgesContext(DbContextOptions options)
+            : base(options)
+        { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            FillTestData(modelBuilder);
+        }
+        private void FillTestData(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<BridgesPoint>().HasData(
+                new
+                {
+                    Id = 1L,
+                    Name = "Мост пешеходный «1905 года»",
+                    Location = "Дружинниковская улица",
+                    CrossRiver = "пруд в парке стадиона Красная Пресня",
+                    Year = "1981"
+                },
+                new
+                {
+                    Id = 2L, 
+                    Name = "Мост пешеходный «Андреевский»",
+                    Location = "1-я Фрунзенская улица",
+                    CrossRiver = "река Москва",
+                    Year = "2000"
+
+                },
+                new
+                {
+                    Id = 3L,
+                    Name = "Мост пешеходный «Балочный»",
+                    Location = "улица Пырьева, дом 24",
+                    CrossRiver = "река Сетунь", 
+                    Year = "1956"
+
+                },
+                new
+                {
+                    Id = 4L, 
+                    Name = "Мост пешеходный «Бауманский-1»",
+                    Location = "городок имени Баумана",
+                    CrossRiver = "пруд Серебряно-Виноградный",
+                    Year = "1964"
+ 
+                }
+            );
+        }
+    }
+}
