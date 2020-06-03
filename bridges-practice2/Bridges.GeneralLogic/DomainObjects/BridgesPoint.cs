70c92b71ab9d64cd9be0e0ae711ac29134859762
Oikswt <50925610+neespi@users.noreply.github.com>
2020-06-04 02:01:33 +0300

    Add files via upload

---
+++ bridges-practice2/Bridges.GeneralLogic/DomainObjects/BridgesPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridges.DomainObjects
+{
+  public class BridgesPoint : DomainObject
+    {
+  
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public string CrossRiver { get; set; }
+
+        public string Year { get; set; }
+    
+    }
+}
